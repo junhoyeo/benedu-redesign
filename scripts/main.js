e65780738f0b9484c660d47a257b2fc913ce7b15
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-10-27 19:08:03 +0900

    feat(main): state bar animation

--- scripts/main.js
+++ scripts/main.js
@@ -37,6 +37,86 @@ countScore.start()
 const countRank = new CountUp('countup-rank', 0, 56, 0, 1.5)
 countRank.start()
 
+const statsBarTimeline = anime.timeline({
+  direction: 'alternate',
+  delay: function(_, i) { return i * 120 },
+  loop: false,
+  easing: 'cubicBezier(.5, .05, .1, .3)'
+})
+
+statsBarTimeline.add({
+  targets: '#bar-1',
+  height: 320,
+  transform: 'translate(768 490)',
+  duration: 380,
+  easing: 'easeInOutSine',
+  // 171, 490
+}).add({
+  targets: '#bar-2',
+  height: 218,
+  transform: 'translate(875 550)',
+  duration: 380,
+  easing: 'easeInOutSine',
+  // 316, 422
+}, '-=220').add({
+  targets: '#bar-3',
+  height: 588,
+  transform: 'translate(982 310)',
+  duration: 380,
+  easing: 'easeInOutSine',
+  // 259 479
+}, '-=220')
+.add({
+  targets: '#bar-4',
+  height: 380,
+  transform: 'translate(1089 410)',
+  duration: 380,
+  easing: 'easeInOutSine',
+  // 375 363
+}, '-=220')
+.add({
+  targets: '#bar-5',
+  height: 300,
+  transform: 'translate(1196 480)',
+  duration: 380,
+  easing: 'easeInOutSine',
+}, '-=220')
+.add({
+  targets: '#bar-1',
+  height: 371,
+  transform: 'translate(768 490)',
+  duration: 380,
+  easing: 'easeInOutSine',
+}, '-=300').add({
+  targets: '#bar-3',
+  height: 259,
+  transform: 'translate(982 479)',
+  duration: 380,
+  easing: 'easeInOutSine',
+}, '-=300').add({
+  targets: '#bar-2',
+  height: 316,
+  transform: 'translate(875 422)',
+  duration: 380,
+  easing: 'easeInOutSine',
+}, '-=300')
+.add({
+  targets: '#bar-4',
+  height: 375,
+  transform: 'translate(1089 363)',
+  duration: 380,
+  easing: 'easeInOutSine',
+}, '-=300')
+.add({
+  targets: '#bar-5',
+  height: 505,
+  transform: 'translate(1196 233)',
+  duration: 380,
+  easing: 'easeInOutSine',
+}, '-=180')
+
+statsBarTimeline.play()
+
 let isWeatherComplete = false
 const weatherTimeline = anime.timeline({
   direction: 'alternate',
