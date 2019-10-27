0bfaca2ea567fdb72015769826518fa0be6ec855
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-10-27 14:24:37 +0900

    feat(main): countup animation

--- scripts/main.js
+++ scripts/main.js
@@ -14,5 +14,25 @@ const confettiSettings = {
   rotate: false,
   width: 1400,
 }
+
+const confettiCanvas = document.getElementById('stats-canvas')
+confettiCanvas.style.opacity = 0.1
+
 const confetti = new ConfettiGenerator(confettiSettings)
 confetti.render()
+
+let confettiOpacity = 0.1
+const confettiIntervalID = setInterval(function () {
+  confettiCanvas.style.opacity = confettiOpacity
+  confettiOpacity += 0.07
+}, 100);
+
+setTimeout(function () {
+  clearInterval(confettiIntervalID)
+}, 1400);
+
+const countScore = new CountUp('countup-score', 0, 1068, 0, 1.5)
+countScore.start()
+
+const countRank = new CountUp('countup-rank', 0, 56, 0, 1.5)
+countRank.start()
